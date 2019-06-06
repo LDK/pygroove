1f1f48291b4c2935888d94fea0cf6f289ebc7848
LDK <lemondropkid@gmail.com>
2019-06-05 23:24:51 -0700

    Remove unneeded imports from Sequencer

--- src/sequencer.js
+++ src/sequencer.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import cloneDeep from 'lodash/cloneDeep';
 import 'whatwg-fetch';
-import AudioOut from './components/AudioOut.js';
-import Range from './components/Range.js';
 import Pattern from './components/Pattern.js';
 
 class Song extends React.Component {
