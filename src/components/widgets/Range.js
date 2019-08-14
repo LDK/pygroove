460f0df533d680d66b11be349c8d54d304a0541a
LDK <lemondropkid@gmail.com>
2019-08-13 20:14:03 -0700

    Clicking label on a numeric range slider will open a text input

--- src/components/widgets/Range.js
+++ src/components/widgets/Range.js
@@ -7,19 +7,22 @@ class Range extends React.Component {
 	constructor(props) {
 		super(props);
 		this.callback = this.callback.bind(this);
+		this.toggleMode = this.toggleMode.bind(this);
 		this.state = {
 			value: props.value || 0,
+			mode: props.mode || 'range',
 			displayValue: props.displayValue || null
 		};
 	}
-	
 	callback(event) {
 		this.setState({ value: event.target.value });
 		this.props.callback(event.target.value,this.props.params || {});
 	}
-	
-	render() {
-		const { range } = this.props;
+	toggleMode() {
+		var mode = (this.state.mode == 'range') ? 'text' : 'range';
+		this.setState({mode: mode});
+	}
+	renderRange(range) {
 		return (
 			<div className={"meter " + (this.props.className || '')}>
 				<input className={this.props.inputClass || ''} type="range"
@@ -32,9 +35,45 @@ class Range extends React.Component {
 					onChange={this.callback}
 					tabIndex="-1" >
 				</input>
-				<span className={'meter-display ' + (this.props.inputClass || '') + ' ' + (this.props.meterClass || '')}>{this.props.value || 0}</span>
+				<span onClick={this.toggleMode} className={'meter-display ' + (this.props.inputClass || '') + ' ' + (this.props.meterClass || '')}>{this.props.value || 0}</span>
 			</div>
-		)
+		);
+	}
+	renderText(range) {
+		return (
+			<div className={"meter " + (this.props.className || '')}>
+				<input className={this.props.inputClass || ''} type="range"
+					value={this.props.value || 0}
+					orient={this.props.orient || 'horizontal'}
+					min={this.props.min || 0}
+					max={this.props.max || 100}
+					step={this.props.step || 1}
+					name={this.props.inputName}
+					onChange={this.callback}
+					tabIndex="-1" >
+				</input>
+				<input type="text" 
+					onChange={this.callback} 
+					className={this.props.textInputClass || 'meter-display col-4 px-0 mx-2'}
+					value={this.state.value || 0} 
+					name={this.props.inputName} 
+					onBlur={this.toggleMode} 
+				/>
+			</div>
+		);
+	}
+	render() {
+		const { range } = this.props;
+		switch (this.state.mode) {
+			case 'text':
+				var output = this.renderText(range);
+				break;
+			case 'range':
+			default:
+				var output = this.renderRange(range);
+				break;
+		}
+		return output;
 	}
 }
 // ========================================
