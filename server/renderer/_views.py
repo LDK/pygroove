82c576c052a6dcf6453790e5da47cacb66989cdc
LDK <lemondropkid@gmail.com>
2024-01-25 19:46:02 -0800

    Split pattern off into its own view file, with routes for rendering previews of pattern or piano roll content

--- server/renderer/_views.py
+++ server/renderer/_views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .views.user import *
 from .views.sample import *
 from .views.song import *
+from .views.pattern import *
 
 def index(request):
   return render(request, 'renderer/index.html')
